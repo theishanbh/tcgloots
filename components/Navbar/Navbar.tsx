fc2fff76d88c740f880739adf87d1e3fa76aa7a2
Ishan <theishanbh@gmail.com>
2022-02-23 00:04:38 +0530

    navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -54,10 +54,11 @@ const Navbar = () => {
                                             href={item.href}
                                             className={classNames(
                                             item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'px-3 py-2 rounded-md text-sm font-medium'
+                                            'px-3 py-2 rounded-md text-sm font-medium inline-flex'
                                             )}
                                             aria-current={item.current ? 'page' : undefined}
                                         >
+                                            {/* <i src="./assets/giveaway.svg" alt="" className='h-2/4'/> {item.name} */}
                                             {item.name}
                                         </a> :
                                         <a
