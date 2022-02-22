bd5fe0b904067c2484dd232997a1048b733ca436
Ishan <theishanbh@gmail.com>
2022-02-23 00:33:58 +0530

    welcome to tcgloots started

---
+++ components/WelcomeTCGLoots/WelcomeTCGLoots.tsx
@@ -0,0 +1,18 @@
+const WelcomeTCGLoots = () => {
+  return (
+    <div className=" bg-slate-500">
+        <div className="max-w-7xl mx-auto ">
+            <div className="py-4">
+                <h1 className=" text-3xl underline-offset-4 hover:text-regal-blue underline">
+                    Welcome to TCGLoots
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex necessitatibus nam voluptates quis at sit possimus sequi inventore numquam? Dolore sequi labore nulla? Odio sed, voluptatem, deserunt id est neque perferendis ea voluptatum quisquam quae mollitia!
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomeTCGLoots
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar/Navbar'
 import ProductTracker from '../components/ProductTracker/ProductTracker'
+import WelcomeTCGLoots from '../components/WelcomeTCGLoots/WelcomeTCGLoots'
 
 const Home: NextPage = () => {
   return (
     <div>
       <div className=' text-center'>
         <Navbar></Navbar>
-        <ProductTracker></ProductTracker>
+        <WelcomeTCGLoots></WelcomeTCGLoots>
       </div>
     </div>
   )
