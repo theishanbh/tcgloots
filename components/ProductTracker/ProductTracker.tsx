1804334f24be33d70da37216df777ff45ea49557
Monarch <vamsivishwanath@gmail.com>
2022-02-23 00:01:59 +0530

    after second commit, added red hello

---
+++ components/ProductTracker/ProductTracker.tsx
@@ -0,0 +1,12 @@
+const ProductTracker = () =>{
+
+    return(
+        <div>
+            <div>
+                <h1 className="text-lg text-red-600">Hello!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ProductTracker;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar/Navbar'
+import ProductTracker from '../components/ProductTracker/ProductTracker'
 
 const Home: NextPage = () => {
   return (
     <div>
       <div className=' text-center'>
         <Navbar></Navbar>
+        <ProductTracker></ProductTracker>
       </div>
     </div>
   )
