29629f44c24d2ba530bfc2576a33e8bb9fe6771c
Ishan <theishanbh@gmail.com>
2022-02-23 22:10:56 +0530

    welcome page half finished

--- components/Homepage/HeroSection/WelcomeTCGLoots.tsx
+++ components/Homepage/HeroSection/WelcomeTCGLoots.tsx
@@ -1,10 +1,15 @@
 const WelcomeTCGLoots = () => {
   return (
     <div className=" bg-gradient-to-r from-colors-grey-400 to-colors-grey-500">
-        <div className="">
-            <div className=" bg-colors-white w-3/5 mx-auto h-4/5">
-                <h1 className=" font-jost font-semibold text-3xl underline underline-offset-8 decoration-solid decoration-colors-yellow-primary">Welcome to TCGLOOTS!</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente asperiores sed quod qui recusandae, deserunt quam veritatis at beatae vitae repellat officia officiis, porro quaerat est obcaecati alias temporibus nihil possimus molestiae impedit facilis. Aliquid, expedita quo.</p>
+        <div className="py-8 px-32">
+            <div className="bg-colors-white mx-auto p-8">
+                <div>
+                  <h1 className=" font-jost font-semibold text-3xl underline underline-offset-8 decoration-solid decoration-colors-yellow-primary">Welcome to TCGLOOTS!</h1>
+                  <p className="font-jost text-xl pt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente asperiores sed quod qui recusandae, deserunt quam veritatis at beatae vitae repellat officia officiis, porro quaerat est obcaecati alias temporibus nihil possimus molestiae impedit facilis. Aliquid, expedita quo.</p>
+                </div>
+                <div>
+                  
+                </div>
             </div>
         </div>
     </div>
