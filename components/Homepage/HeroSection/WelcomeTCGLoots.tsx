1b9b86fcba8996320d4bf901ad8dc919705af243
Ishan <theishanbh@gmail.com>
2022-02-24 16:18:10 +0530

    welcome screen completed

--- components/Homepage/HeroSection/WelcomeTCGLoots.tsx
+++ components/Homepage/HeroSection/WelcomeTCGLoots.tsx
@@ -2,15 +2,15 @@ const WelcomeTCGLoots = () => {
   return (
     <div className=" bg-gradient-to-r from-colors-grey-400 to-colors-grey-500">
         <div className="py-8 max-w-7xl mx-auto">
-            <div className="bg-colors-white mx-auto p-8">
+            <div className="bg-colors-white mx-auto p-8 flex">
                 {/* info welcome tcgLoots */}
-                <div>
+                <div className="flex-4">
                   <h1 className=" font-jost font-semibold text-3xl underline underline-offset-8 decoration-solid decoration-colors-yellow-primary">Welcome to TCGLOOTS!</h1>
                   <p className="font-jost text-xl pt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente asperiores sed quod qui recusandae, deserunt quam veritatis at beatae vitae repellat officia officiis, porro quaerat est obcaecati alias temporibus nihil possimus molestiae impedit facilis. Aliquid, expedita quo.</p>
                 </div>
                 {/* Image */}
-                <div>
-
+                <div className="flex-1">
+                  <img src="./assets/homepage/welcometgcloots/images.png" alt="" />
                 </div>
             </div>
         </div>
