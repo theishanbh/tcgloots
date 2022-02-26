7e207e80060f123ee6654249231c2de446951598
Ishan <theishanbh@gmail.com>
2022-02-26 11:17:05 +0530

    folder structure

---
+++ pages/contactus/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+
+const ContactUs = () => {
+  return (
+    <div>
+        <div>
+            <Navbar></Navbar>
+            <div className='mx-auto max-w-7xl'>
+                <div className='flex overflow-hidden items-center justify-center'>
+
+                </div>
+            </div>
+            <Footer></Footer>
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
