0e776772bff6d5749c56e36744a2a561465a3773
Ishan <theishanbh@gmail.com>
2022-02-26 17:42:17 +0530

    apostrophe removed

--- pages/default.tsx
+++ pages/default.tsx
@@ -6,7 +6,7 @@ const Default = () => {
         <div className=' max-w-7xl mx-auto text-center h-screen'>
             <div className='h-full flex flex-col my-auto justify-center items-center'>
                 <h1 className=' text-8xl font-bold'>404</h1>
-                <p className=' text-2xl w-full py-4'>We couldn't find the page you were looking for. Maybe our FAQ can help?</p>
+                <p className=' text-2xl w-full py-4'>We couldn&apos;t find the page you were looking for. Maybe our FAQ can help?</p>
                 <div>
                     <button className=" my-2 border text-blue-primary bg-white border-blue-primary font-jost text-xl rounded-md px-16 py-2">Back To Home</button>
                 </div>
