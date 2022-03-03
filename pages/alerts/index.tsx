79d3aecee1d66bbcb81d927591f1fa2e475dcf29
Ishan <theishanbh@gmail.com>
2022-03-03 13:33:11 +0530

    alerts draft finished

--- pages/alerts/index.tsx
+++ pages/alerts/index.tsx
@@ -14,6 +14,7 @@ const Alerts = () => {
                         <div>
                             {/* Alerts Notificatiosn */}
                             <div className='flex flex-col'>
+                                {/* heading */}
                                 <div className='flex items-center justify-between'>
                                     <div className='relative flex items-center justify-center'>
                                         <Image
@@ -45,8 +46,84 @@ const Alerts = () => {
                                 </div>
                                 <div className='p-8'>
                                     <div className='border-4 rounded-xl border-yellow-primary p-4'>
-                                You’ll be notified when the following products are back in stock. Manage notification preferences in Settings. Not sure if your alerts are working? Watch this Demo product to test out receiving them every minute. 
+                                        You’ll be notified when the following products are back in stock. Manage notification preferences in Settings. Not sure if your alerts are working? Watch this Demo product to test out receiving them every minute. 
                                     </div>
+                                </div>
+                            </div>
+                            {/* Table */}
+                            <div className="">
+                                <div>
+                                <table className=' table-auto w-full'>
+                                    <thead>
+                                        <tr>
+                                            <th className=' text-left'>Tracked products</th>
+                                            <th>Last price</th>
+                                            <th>Last stock</th>
+                                            <th className=' justify-between flex items-center'>
+                                                <span>Constantly checking for stock</span> 
+                                                <div className='relative'>
+                                                    <Image
+                                                        src={require("../../assets/alerts/loading.png")}
+                                                    />
+                                                </div>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <div className='flex'>
+                                                        <div className="relative">
+                                                            <Image
+                                                                src={require("../../assets/alerts/default.png")}
+                                                            />
+                                                        </div>
+                                                        <span>Sony PlayStation 5 Console (PS5)</span>
+                                                    </div>
+                                                </td>
+                                                <td>€ 499.99</td>
+                                                <td>12 Dec 08:08</td>
+                                                <td>
+                                                    <button className='border flex font-jost text-white bg-blue-primary text-base font-medium rounded-md px-12 py-2 ml-2'>
+                                                        <div className='relative'>
+                                                            <Image
+                                                                src={require("../../assets/alerts/checkCircle.svg")}
+                                                            ></Image>
+                                                        </div>
+                                                        <span>
+                                                            IN STOCK
+                                                        </span>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div className='flex'>
+                                                        <div className="relative">
+                                                            <Image
+                                                                src={require("../../assets/alerts/default.png")}
+                                                            />
+                                                        </div>
+                                                        <span>Sony PlayStation 5 Console (PS5)</span>
+                                                    </div>
+                                                </td>
+                                                <td>€ 499.99</td>
+                                                <td>12 Dec 08:08</td>
+                                                <td>
+                                                    <button className='border flex font-jost text-white bg-blue-primary text-base font-medium rounded-md px-12 py-2 ml-2'>
+                                                        <div className='relative'>
+                                                            <Image
+                                                                src={require("../../assets/alerts/checkCircle.svg")}
+                                                            ></Image>
+                                                        </div>
+                                                        <span>
+                                                            IN STOCK
+                                                        </span>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
