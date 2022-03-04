0ef4e4e539f62f66b7da8f16bf1e6a88228557f2
Ishan <theishanbh@gmail.com>
2022-03-04 23:26:58 +0530

    finished draft

---
+++ pages/testing.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar/Navbar'
+
+const Testing = () => {
+  return (
+    <div>
+        <div>
+            <Navbar></Navbar>
+            <div className="mx-auto">
+
+            </div>
+            <Footer></Footer>
+        </div>
+    </div>
+  )
+}
+
+export default Testing
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,45 +4,78 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      '2sm': '.938rem',
+      base: '1rem',
+      lg: '1.125rem',
+      '2lg' : '1.188rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.375rem',
+      '4xl': '1.625rem',
+      '5xl': '1.75rem',
+      '6xl': '1.875rem',
+      '7xl': '2.188rem',
+      '8xl': '2.5rem',
+      '9xl': '5rem',
+    },
     colors: {
       // white
+      transparent : 'transparent',
       white : {
-        'primary' : '#FFFFFF',
+        100 : '#FFFFFF',
       },
       // black
+      black : {
+        100 : "#454545",
+      },
+      grey : {
+        100 : "#555555",
+        200 : "#666666",
+        300 : "#606060",
+        400 : "#888888",
+        500 : "#CCCCCC",
+        600 : "#DDDDDD",
+      },
       // slate
+      slate : {
+        100 : "#EEEEEE",
+        200 : "#F4F4F4",
+        300 : "#F2F4F7",
+        400 : "#F9F9F9",
+        500 : "#FCFCFC",
+      },
       // yellow
+      yellow : {
+        100 : "#FDAE22",
+        200 : "#FFCC30",
+        300 : "#F9D260",
+        400 : "#FCE59F",
+      },
       // brown
+      brown : {
+        100 : "#8D6E00"
+      },
       // red
+      red : {
+        100 : "#C40016",
+      },
       // pink
+      pink : {
+        100 : "#FCE8E8"
+      },
       // blue
+      blue : {
+        100 : "#5865F2",
+        200 : "#1890CF",
+        300 : "#55AEF0",
+      },
       // green
-      // white
-      'white': '#FFFFFF',
-      // yellow
-      'yellow-primary' : '#FFCC30',
-      'yellow-secondary' : '#FCE59F',
-      // grey
-      'grey-primary' : "#888888",
-      'grey-200' : '#EEEEEE',
-      'grey-300' : '#F2F4F7',
-      'grey-400' : '#DDDDDD',
-      'grey-500' : '#CCCCCC',
-      'grey-600' : '#FCFCFC',
-      'grey-700' : '#F4F4F4',
-      'grey-800' : '#F7F8FC',
-      // slate
-      // black
-      'black-100' : "#545454",
-      'black-200' : '#454545',
-      'black-300' : '#606060',
-      // blue
-      'blue-primary' : "#55AEF0",
-      'blue-100' : '#5AA3D9',
-      'blue-200' : '#1890CF',
-      'blue-discord' : '#5865F2',
-      // green
-      'green-primary' : "#82C732"
+      green : {
+        100 : "#82C732",
+      },
     },
     
     extend: {
