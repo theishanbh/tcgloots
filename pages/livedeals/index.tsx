b59645651297443ee89fc203afe987943529ae81
Ishan <theishanbh@gmail.com>
2022-03-04 23:38:12 +0530

    camelcase fixed

--- pages/livedeals/index.tsx
+++ pages/livedeals/index.tsx
@@ -107,7 +107,7 @@ const LiveDeals = () => {
                                 </div>
                                 <div className="flex-3">
                                     <div>
-                                    <button id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation" className="bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown header <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                                    <button id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation" className="bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown header <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19 9l-7 7-7-7"></path></svg></button>
 
                                     <div id="dropdownInformation" className="z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                                     <ul className="py-1" aria-labelledby="dropdownInformationButton">
