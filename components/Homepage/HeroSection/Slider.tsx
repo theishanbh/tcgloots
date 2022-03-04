eae10b0a568962c8f8b0e52fadc4f64b08f244db
Ishan <theishanbh@gmail.com>
2022-03-05 02:14:06 +0530

    slider

--- components/Homepage/HeroSection/Slider.tsx
+++ components/Homepage/HeroSection/Slider.tsx
@@ -1,8 +1,143 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className='py-4 bg-slate-300'>
+      <div className="flex overflow-hidden">
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        {/* slider item */}
+        <div className='px-2 flex-grow-0 flex-shrink-0'>
+          <div className="flex items-center justify-center border border-grey-600 rounded-xl bg-slate-100 py-1">
+            <div className='relative px-2'>
+              <Image
+                src={require("../../../assets/homepage/herosection/default.png")}
+              />
+            </div>
+            <div className='px-4'>
+              <span>Ilyes101</span>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
