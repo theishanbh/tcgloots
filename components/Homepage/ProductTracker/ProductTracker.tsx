a59d2c134fcc964755f0391b3931320286e1e38a
Ishan <theishanbh@gmail.com>
2022-03-05 02:54:51 +0530

    product tracker componenet finished

--- components/Homepage/ProductTracker/ProductTracker.tsx
+++ components/Homepage/ProductTracker/ProductTracker.tsx
@@ -4,98 +4,107 @@ const ProductTracker = () =>{
 
     return(
         <div className="font-jost">
-            <div className="bg-grey-600">
+            <div className="bg-slate-500">
                 <div className="max-w-7xl mx-auto">
-                    <h1 className=" font-jost text-4xl font-semibold text-center py-12">Products Tracker</h1>
+                    <h1 className=" font-jost text-8xl text-black-200 font-semibold text-center py-12">Products Tracker</h1>
                     <div className="flex flex-wrap">
                         <div className="w-1/6">
-                            <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                            <div className=" bg-white h-4/5 p-4 rounded-xl">
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         <div className="w-1/6">
                             <div className=" bg-white h-4/5 p-4 ">
-                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-xl"/>
+                                <img src="./assets/homepage/producttracker/image2.png" alt="" className="h-full w-full p-2 shadow-full rounded-lg"/>
                             </div>
-                            <p className=" text-lg text-center h-1/5">Lorem, ipsum dolor</p>
+                            <p className=" text-lg text-black-100 text-center h-1/5">Lorem, ipsum dolor</p>
                         </div>
                         
                     </div>
-                    <div>
-                        <button className=" border font-jost text-xl rounded-md px-16 py-2">View All</button>
+                    {/* button */}
+                    <div className=" text-right bg-white-100 py-8">
+                        <button className=" border border-grey-600 font-jost text-xl text-black-100 rounded-md px-16 py-2 shadow-full">View All</button>
                     </div>
                 </div>
                 
             </div>
-            <div className="bg-grey-700">
+            <div className=" bg-gradient-to-r from-slate-400 to-slate-100">
                 <div className="max-w-7xl mx-auto py-8">
                     {/* image */}
-                    <div></div>
-                    {/*  */}
-                    <div className="text-center">
-                        <p className=" text-3xl font-light">Want to suggest a product for us to track?</p>
-                        <button className=" bg-blue-primary text-white inline-flex hover:bg-blue-100 mt-6 text-white font-bold py-2 px-4 rounded"> Submit a suggestion <img src="./assets/rightarrow.svg" className="ml-2"/> </button>
+                    <div className="flex items-center justify-center">
+                        <div>
+                            <div>
+                                <Image
+                                    src={require("../../../assets/homepage/producttracker/groupimages.png")}
+                                ></Image>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className="text-center px-12">
+                            <p className=" text-4xl font-light">Want to suggest a product for us to track?</p>
+                            <button className=" bg-blue-300 text-white-100 text-xl inline-flex hover:bg-blue-100 mt-6 text-white font-bold py-2 px-4 rounded"> Submit a suggestion <img src="./assets/rightarrow.svg" className="ml-2"/> </button>
+                        </div>
                     </div>
                 </div>
             </div>                
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,7 @@ module.exports = {
       // black
       black : {
         100 : "#454545",
+        200 : "#545454",
       },
       grey : {
         100 : "#555555",
@@ -92,6 +93,19 @@ module.exports = {
         '7' : '7 7 0%',
         '8' : '8 8 0%',
         '9' : '9 9 0%',
+      },
+      boxShadow: {
+        full : "0px 0px 6px 6px rgba(0, 0, 0, 0.04)",
+        xs: "0 0 0 1px rgba(0, 0, 0, 0.16)",
+        sm: "0 1px 2px 0 rgba(0, 0, 0, 0.16)",
+        default: "0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.03)",
+        md: "0 4px 6px -1px rgba(0, 0, 0, 0.04), 0 2px 4px -1px rgba(0, 0, 0, 0.03)",
+        lg: "0 10px 15px -3px rgba(0, 0, 0, 0.04), 0 4px 6px -2px rgba(0, 0, 0, 0.02)",
+        xl: "0 20px 25px -5px rgba(0, 0, 0, 0.12), 0 10px 10px -5px rgba(0, 0, 0, 0.02)",
+        "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.15)",
+        inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.04)",
+        outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+        none: "none",
       },
       
     },
