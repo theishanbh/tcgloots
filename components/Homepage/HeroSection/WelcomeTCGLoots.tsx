4c86380925c95dd6fea311260df48f9d7473d801
Ishan <theishanbh@gmail.com>
2022-03-05 02:22:51 +0530

    tcgloots welcome finished

--- components/Homepage/HeroSection/WelcomeTCGLoots.tsx
+++ components/Homepage/HeroSection/WelcomeTCGLoots.tsx
@@ -1,11 +1,11 @@
 const WelcomeTCGLoots = () => {
   return (
-    <div className=" bg-gradient-to-r from-grey-400 to-grey-500">
+    <div className=" bg-gradient-to-b from-grey-600 to-grey-500">
         <div className="py-32 px-28 max-w-7xl mx-auto relative">
-            <div className=" relative bg-white mx-auto p-8 flex">
+            <div className=" relative bg-slate-200 mx-auto p-8 flex rounded-md">
                 {/* info welcome tcgLoots */}
                 <div className="flex-4">
-                  <h1 className=" font-jost font-semibold text-3xl underline underline-offset-8 decoration-solid decoration-yellow-primary">Welcome to TCGLOOTS!</h1>
+                  <h1 className=" font-jost font-semibold text-6xl underline underline-offset-8 decoration-solid decoration-4 decoration-yellow-200">Welcome to TCGLOOTS!</h1>
                   <p className="font-jost text-xl pt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente asperiores sed quod qui recusandae, deserunt quam veritatis at beatae vitae repellat officia officiis, porro quaerat est obcaecati alias temporibus nihil possimus molestiae impedit facilis. Aliquid, expedita quo.</p>
                 </div>
                 {/* Image */}
