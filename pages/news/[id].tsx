89167b9e3573f7ceecb551463dfca1a225cfde33
Ishan Bhardwaj <theishanbh@gmail.com>
2022-03-11 19:07:42 +0530

    left section done

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -9,17 +9,19 @@ const Post = ({blog} : {blog:any}) => {
                 <Navbar></Navbar>
                 <div className="flex py-8">
                     {/* Articles */}
-                    <div className="flex-2">
+                    <div className="flex-2 pr-20">
                         <div className="flex flex-col">
-                            <h1 className=" text-4xl font-semibold">Lorem ipsum dolor sit amet</h1>
-                            <span className=" font-light text-xl text-black-200 py-4">12 Dec 2021</span>
+                            <h1 className=" text-7xl text-black-100 font-semibold">Lorem ipsum dolor sit amet</h1>
+                            <span className=" font-light text-2xl text-black-200 py-4">12 Dec 2021</span>
                             {/* img container */}
-                            <div className="py-4">
+                            <div className="py-4 relative">
                                 <Image
-                                    src={require('../../assets/image1.png')}
+                                    src={require('../../assets/news/image1.png')}
+                                    layout="responsive"
+                                    className=" rounded-xl"
                                 />
                             </div>
-                            <div className=" text-xl">
+                            <div className=" text-2xl">
                                 <p className="py-4">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                                 </p>
