972a65811856a845f7a88c80f715d8faf3d9eaa3
ishanbh <theishanbh@gmail.com>
2022-03-17 14:22:23 +0530

    footer added

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
 import NewsHero from '../../components/News/News/NewsHero'
 import NewsArticles from '../../components/News/NewsArticles/NewsArticles'
