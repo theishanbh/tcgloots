9f8821710f09f7d03d2ceac4982e5817b2ffbee5
ishanbh <theishanbh@gmail.com>
2022-03-18 17:32:57 +0530

    heading underline done

--- components/Homepage/HeroSection/WelcomeTCGLoots.tsx
+++ components/Homepage/HeroSection/WelcomeTCGLoots.tsx
@@ -5,9 +5,8 @@ const WelcomeTCGLoots = () => {
             <div className=" relative bg-slate-200 mx-auto p-8 flex rounded-md">
                 {/* info welcome tcgLoots */}
                 <div className="flex-4">
-                  <div className="flex flex-col">
-                    <h1 className=" font-jost inline font-semibold text-6xl underline underline-offset-8 decoration-solid decoration-4 decoration-yellow-200">Welcome to TCGLOOTS!</h1>
-                    <div className=""/>
+                  <div className=" border-b-yellow-200 border-b-4 w-max pb-3">
+                    <h1 className=" font-jost inline font-semibold text-6xl">Welcome to TCGLOOTS!</h1>
                   </div>
                   
                   <p className="font-jost text-xl pt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente asperiores sed quod qui recusandae, deserunt quam veritatis at beatae vitae repellat officia officiis, porro quaerat est obcaecati alias temporibus nihil possimus molestiae impedit facilis. Aliquid, expedita quo.</p>
