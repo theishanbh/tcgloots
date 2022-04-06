e5851521f40ec2309bf7306bc22fb70069ceca44
Ishan Bhardwaj <theishanbh@gmail.com>
2022-04-06 22:20:33 +0530

    new articles completed

--- components/News/News/NewsHero.tsx
+++ components/News/News/NewsHero.tsx
@@ -3,29 +3,33 @@ import React from 'react'
 
 const NewsHero = () => {
   return (
-    <div className="bg-slate-500 shadow-xl relative">
+    <div className="bg-slate-500 shadow-xl relative font-jost">
       <div className="bg-[url('/assets/news/news/background.png')] bg-bottom bg-cover bg-blend-darken max-w-7xl mx-auto bg-no-repeat  py-12">
           <div className='mx-auto max-w-7xl font-jost bg-transparent'>
               <h1 className="font-jost text-8xl text-black-200 font-semibold text-center">News</h1>
-              <div className='flex flex-wrap py-8'>
-                <div className='flex flex-col w-1/4 px-4 '>
+              <div className='flex flex-grow py-8'>
+                <div className='flex flex-col px-4 '>
                   <div className='bg-slate-500 shadow-full rounded-md'>
                     {/* image container */}
-                    <div className=''>
-                      <img src="./assets/news/news/image1.png" alt="" className='h-full w-full' />
+                    <div className='relative'>
+                      <Image
+                        src={require("../../../assets/news/image1.png")}
+                        layout="intrinsic"
+                      />
+
                     </div>
                     {/* text container */}
-                    <div className='flex flex-col py-2 px-4'>
-                      <p className=' font-medium text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
-                      <span className=' text-grey-primary text-lg py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
+                    <div className='flex flex-col py-1 md:py-2 px-4'>
+                      <p className=' font-medium text-xl 2md:text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
+                      <span className=' text-grey-primary text-base lg:text-lg pt-2 pb-4 md:py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
                       <div className='flex justify-between items-center'>
-                        <p className=' text-2sm'>1 Dec 2021</p>
-                        <button className='border bg-grey-600 text-grey-400 font-jost text-base rounded-md px-1'>Category</button>
+                        <p className=' text-sm md:text-2sm'>1 Dec 2021</p>
+                        <button className='border bg-grey-600 text-grey-400 font-jost text-sm md:text-base rounded-md px-1'>Category</button>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className='flex flex-col w-1/4 px-4 '>
+                <div className='flex flex-col px-4 '>
                   <div className='bg-slate-500 shadow-full rounded-xl'>
                     {/* image container */}
                     <div className=''>
@@ -37,17 +41,17 @@ const NewsHero = () => {
                       </div>
                     </div>
                     {/* text container */}
-                    <div className='flex flex-col py-2 px-4'>
-                      <p className=' font-medium text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
-                      <span className=' text-grey-primary text-lg py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
+                    <div className='flex flex-col py-1 md:py-2 px-4'>
+                      <p className=' font-medium text-xl 2md:text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
+                      <span className='text-grey-primary text-base lg:text-lg pt-2 pb-4 md:py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
                       <div className='flex justify-between items-center'>
-                        <p className=' text-2sm'>1 Dec 2021</p>
-                        <button className='border bg-yellow-500 text-brown-100 font-jost text-base rounded-md px-1'>Featured</button>
+                        <p className='text-sm md:text-2sm'>1 Dec 2021</p>
+                        <button className='border bg-yellow-500 text-brown-100 font-jost text-sm md:text-base rounded-md px-1'>Featured</button>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className='flex flex-col w-1/4 px-4 '>
+                <div className='flex flex-col px-4 '>
                   <div className='bg-slate-500 shadow-full rounded-xl'>
                     {/* image container */}
                     <div className=''>
@@ -59,17 +63,17 @@ const NewsHero = () => {
                       </div>
                     </div>
                     {/* text container */}
-                    <div className='flex flex-col py-2 px-4'>
-                      <p className=' font-medium text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
-                      <span className=' text-grey-primary text-lg py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
+                    <div className='flex flex-col py-1 md:py-2 px-4'>
+                      <p className=' font-medium text-xl 2md:text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
+                      <span className='text-grey-primary text-base lg:text-lg pt-2 pb-4 md:py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
                       <div className='flex justify-between items-center'>
-                        <p className=' text-2sm'>1 Dec 2021</p>
-                        <button className='border bg-slate-200 text-blue-300 font-jost text-base rounded-md px-1'>Category</button>
+                        <p className='text-sm md:text-2sm'>1 Dec 2021</p>
+                        <button className='border bg-slate-200 text-blue-300 font-jost text-sm md:text-base rounded-md px-1'>Category</button>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className='flex flex-col w-1/4 px-4 '>
+                <div className='flex flex-col px-4 '>
                   <div className='bg-slate-500 shadow-full rounded-md'>
                     {/* image container */}
                     <div className=''>
@@ -81,12 +85,12 @@ const NewsHero = () => {
                         </div>
                     </div>
                     {/* text container */}
-                    <div className='flex flex-col py-2 px-4'>
-                      <p className=' font-medium text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
-                      <span className=' text-grey-primary text-lg py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
+                    <div className='flex flex-col py-1 md:py-2 px-4'>
+                      <p className=' font-medium text-xl 2md:text-2xl text-black-100'>Lorem ipsum dolor sit amet</p>
+                      <span className='text-grey-primary text-base lg:text-lg pt-2 pb-4 md:py-8 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</span>
                       <div className='flex justify-between items-center'>
-                        <p className=' text-2sm'>1 Dec 2021</p>
-                        <button className='border bg-yellow-500 text-brown-100 font-jost text-base rounded-md px-1'>Featured</button>
+                        <p className='text-sm md:text-2sm'>1 Dec 2021</p>
+                        <button className='border bg-yellow-500 text-brown-100 font-jost text-sm md:text-base rounded-md px-1'>Featured</button>
                       </div>
                     </div>
                   </div>
--- components/News/NewsArticles/NewsArticles.tsx
+++ components/News/NewsArticles/NewsArticles.tsx
@@ -7,7 +7,7 @@ const NewsArticles = () => {
     <div className="bg-slate-300">
         <div className=' max-w-7xl mx-auto font-jost py-12'>
             {/* Categories */}
-            <div className=' bg-white shadow-lg bg-white-100 p-4 border-4 rounded-lg border-grey-600'>
+            <div className=' hidden md:block bg-white shadow-lg bg-white-100 p-4 border-4 rounded-lg border-grey-600'>
                 <div className='flex text-black-100'>
                     <a className=' px-8 text-xl border-r border-grey-600'>ALL</a>
                     <a className=' px-8 text-xl border-r border-grey-600'>FEATURED</a>
@@ -20,23 +20,23 @@ const NewsArticles = () => {
               <div className="flex flex-row px-2 xl:px-0">
                   <div className="flex flex-col flex-3">
                     <div className="flex items-start flex-row justify-start border-solid border-b-4 border-grey-600 py-10" >
-                        <div 
-                          className="relative justify-center items-center my-auto flex-shrink-0 overflow-hidden"
-                        >
+                        <div className="relative flex-1 md:flex-initial justify-center items-center my-auto flex-shrink-0 overflow-hidden">
                             <Image
                               src={require("../../../assets/news/image1.png")}
                               objectFit="cover"
+                              layout = "responsive"
                               className=' rounded-lg'
                             />
                         </div>
-                        <div className="flex flex-col h-full ml-4 w-auto">
+                        <div className="flex flex-2 md:flex-initial flex-col h-full ml-4 w-auto">
                             <Link
                               href={"/news/1"}
                             >
-                            <span className=" text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
+                            <span className=" text-base md:text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-0 md:pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
                             </Link>
-                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
-                            <div className=" text-2sm text-black-100 mt-auto text-left pt-3 pb-3"> 1 Dec 2021 </div>
+                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden hidden md:block"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
+                            <div className=" md:text-2sm text-grey-400 md:text-black-100 mt-0 md:mt-auto text-left py-0 md:py-3"> 1 Dec 2021 </div>
+                            <div className=" text-base block md:hidden text-black-100 mt-auto text-left"> Read More &gt; </div>
                         </div>
                       </div>
                     </div>
@@ -44,23 +44,23 @@ const NewsArticles = () => {
               <div className="flex flex-row px-2 xl:px-0">
                   <div className="flex flex-col flex-3">
                     <div className="flex items-start flex-row justify-start border-solid border-b-4 border-grey-600 py-10" >
-                        <div 
-                          className="relative justify-center items-center my-auto flex-shrink-0 overflow-hidden"
-                        >
+                        <div className="relative flex-1 md:flex-initial justify-center items-center my-auto flex-shrink-0 overflow-hidden">
                             <Image
                               src={require("../../../assets/news/image1.png")}
                               objectFit="cover"
+                              layout = "responsive"
                               className=' rounded-lg'
                             />
                         </div>
-                        <div className="flex flex-col h-full ml-4 w-auto">
+                        <div className="flex flex-2 md:flex-initial flex-col h-full ml-4 w-auto">
                             <Link
                               href={"/news/1"}
                             >
-                            <span className=" text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
+                            <span className=" text-base md:text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-0 md:pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
                             </Link>
-                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
-                            <div className=" text-2sm text-black-100 mt-auto text-left pt-3 pb-3"> 1 Dec 2021 </div>
+                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden hidden md:block"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
+                            <div className=" md:text-2sm text-grey-400 md:text-black-100 mt-0 md:mt-auto text-left py-0 md:py-3"> 1 Dec 2021 </div>
+                            <div className=" text-base block md:hidden text-black-100 mt-auto text-left"> Read More &gt; </div>
                         </div>
                       </div>
                     </div>
@@ -68,23 +68,23 @@ const NewsArticles = () => {
               <div className="flex flex-row px-2 xl:px-0">
                   <div className="flex flex-col flex-3">
                     <div className="flex items-start flex-row justify-start border-solid border-b-4 border-grey-600 py-10" >
-                        <div 
-                          className="relative justify-center items-center my-auto flex-shrink-0 overflow-hidden"
-                        >
+                        <div className="relative flex-1 md:flex-initial justify-center items-center my-auto flex-shrink-0 overflow-hidden">
                             <Image
                               src={require("../../../assets/news/image1.png")}
                               objectFit="cover"
+                              layout = "responsive"
                               className=' rounded-lg'
                             />
                         </div>
-                        <div className="flex flex-col h-full ml-4 w-auto">
+                        <div className="flex flex-2 md:flex-initial flex-col h-full ml-4 w-auto">
                             <Link
                               href={"/news/1"}
                             >
-                            <span className=" text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
+                            <span className=" text-base md:text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-0 md:pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
                             </Link>
-                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
-                            <div className=" text-2sm text-black-100 mt-auto text-left pt-3 pb-3"> 1 Dec 2021 </div>
+                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden hidden md:block"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
+                            <div className=" md:text-2sm text-grey-400 md:text-black-100 mt-0 md:mt-auto text-left py-0 md:py-3"> 1 Dec 2021 </div>
+                            <div className=" text-base block md:hidden text-black-100 mt-auto text-left"> Read More &gt; </div>
                         </div>
                       </div>
                     </div>
@@ -92,23 +92,23 @@ const NewsArticles = () => {
               <div className="flex flex-row px-2 xl:px-0">
                   <div className="flex flex-col flex-3">
                     <div className="flex items-start flex-row justify-start border-solid border-b-4 border-grey-600 py-10" >
-                        <div 
-                          className="relative justify-center items-center my-auto flex-shrink-0 overflow-hidden"
-                        >
+                        <div className="relative flex-1 md:flex-initial justify-center items-center my-auto flex-shrink-0 overflow-hidden">
                             <Image
                               src={require("../../../assets/news/image1.png")}
                               objectFit="cover"
+                              layout = "responsive"
                               className=' rounded-lg'
                             />
                         </div>
-                        <div className="flex flex-col h-full ml-4 w-auto">
+                        <div className="flex flex-2 md:flex-initial flex-col h-full ml-4 w-auto">
                             <Link
                               href={"/news/1"}
                             >
-                            <span className=" text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
+                            <span className=" text-base md:text-6xl font-medium text-black-100 text-left no-underline hover:underline pb-0 md:pb-2 hover:text-blue-700"> Lorem ipsum dolor sit amet </span>
                             </Link>
-                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
-                            <div className=" text-2sm text-black-100 mt-auto text-left pt-3 pb-3"> 1 Dec 2021 </div>
+                            <p className="text-xl font-light text-black-100 text-left text-ellipsis overflow-hidden hidden md:block"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do... </p>
+                            <div className=" md:text-2sm text-grey-400 md:text-black-100 mt-0 md:mt-auto text-left py-0 md:py-3"> 1 Dec 2021 </div>
+                            <div className=" text-base block md:hidden text-black-100 mt-auto text-left"> Read More &gt; </div>
                         </div>
                       </div>
                     </div>
