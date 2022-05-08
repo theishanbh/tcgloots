332d892973258da698949954f5452eb15d08c395
theishanbh <theishanbh@gmail.com>
2022-05-08 23:44:45 +0530

    done

--- components/TrackerPage/PrimaryButtonWithRightIcon.tsx
+++ components/TrackerPage/PrimaryButtonWithRightIcon.tsx
@@ -1,20 +1,22 @@
-import React, { ReactNode } from 'react'
-import Image from 'next/image'
+import React, { ReactNode } from "react";
+import Image from "next/image";
 
 interface props {
-    children: ReactNode
+  title: String;
 }
 export default function PrimaryButtonWithRightIcon(props: props) {
-    return (
-        <button className="bg-blue-300 px-2  font-jost text-sm text-white-100 rounded-lg py-2">
-            <div className="w-48 flex flex-row items-center justify-center space-x-4  ">
-                <div className="font-jost text-xl">{props.children}</div>
-                <div className='my-1'><Image
-                    src={require("../../assets/trackerpage/rightArrow.svg")}
-                    width={10}
-                ></Image></div>
-
-            </div>
-        </button>
-    )
+  return (
+    <button className="bg-blue-300 px-2  font-jost text-sm text-white-100 rounded-lg py-2">
+      <div className="w-48 flex flex-row items-center justify-center space-x-4  ">
+        <div className="font-jost text-xl">{props.title}</div>
+        <div className="my-1 flex justify-center items-center">
+          <Image
+            src={require("../../assets/trackerpage/rightArrow.svg")}
+            width={10}
+            alt="alt"
+          ></Image>
+        </div>
+      </div>
+    </button>
+  );
 }
