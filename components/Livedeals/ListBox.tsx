3e71547980613d1693aa5be9f9c39670de281323
theishanbh <theishanbh@gmail.com>
2022-05-08 23:23:00 +0530

    done

--- components/Livedeals/ListBox.tsx
+++ components/Livedeals/ListBox.tsx
@@ -1,97 +1,96 @@
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-import Image from 'next/image'
-import FlagUS from '../../assets/trackerpage/flag-us.svg'
-import FlagUk from '../../assets/trackerpage/flag-uk.svg';
-import FlagCa from '../../assets/trackerpage/flag-ca.svg';
-import FlagGe from '../../assets/trackerpage/flag-ge.svg';
-import Flagit from '../../assets/trackerpage/flag-it.svg';
-import FlagFr from '../../assets/trackerpage/flag-fr.svg';
-import FlagJa from '../../assets/trackerpage/flag-ja.svg';
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import FlagUS from "../../assets/trackerpage/flag-us.svg";
+import FlagUk from "../../assets/trackerpage/flag-uk.svg";
+import FlagCa from "../../assets/trackerpage/flag-ca.svg";
+import FlagGe from "../../assets/trackerpage/flag-ge.svg";
+import Flagit from "../../assets/trackerpage/flag-it.svg";
+import FlagFr from "../../assets/trackerpage/flag-fr.svg";
+import FlagJa from "../../assets/trackerpage/flag-ja.svg";
 
 const people = [
-    { name: 'United States', imageUrl: FlagUS },
-    { name: 'United Kingdom', imageUrl: FlagUk },
-    { name: 'Canada', imageUrl: FlagCa },
-    { name: 'Deutschland', imageUrl: FlagGe },
-    { name: 'Italia', imageUrl: Flagit },
-    { name: 'France', imageUrl: FlagFr },
-    { name: 'Japan', imageUrl: FlagJa },
-]
+  { name: "United States", imageUrl: FlagUS },
+  { name: "United Kingdom", imageUrl: FlagUk },
+  { name: "Canada", imageUrl: FlagCa },
+  { name: "Deutschland", imageUrl: FlagGe },
+  { name: "Italia", imageUrl: Flagit },
+  { name: "France", imageUrl: FlagFr },
+  { name: "Japan", imageUrl: FlagJa },
+];
 
 export default function ListBox() {
-    const [selected, setSelected] = useState(people[0])
+  const [selected, setSelected] = useState(people[0]);
 
-    return (
-        <div className="z-40 flex flex-1">
-            <Listbox value={selected} onChange={setSelected}>
-                <div className="relative w-full">
-                    <div className="flex flex-1">
-                    <Listbox.Button className="flex flex-row w-full items-center py-2  pr-6 font-jost text-left bg-white-100 rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-grey-500 focus-visible:ring-offset-2 focus-visible:border-white-100 text-xs">
-                        <span className="pl-2 pt-2 block flex-1  ">
-                            <Image
-                                src={
-                                    FlagUS
-                                }
-                                width={20}
-                                height={20}
-                                alt={`flagImage`}
-                            ></Image>
-                        </span>
-                        <div className="block w-full text-center truncate flex-4 pt-1 ">{selected.name}</div>
-                        <span className="absolute inset-y-0 top-4 pr-2 pointer-events-none flex-1 right-0">
-                            <ChevronDownIcon
-                                className="w-5 h-5 ml-2 -mr-1 text-black-100"
-                                aria-hidden="true"
-                            />
-                        </span>
-                    </Listbox.Button>
-                    </div>
-                    <Transition
-                        as={Fragment}
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white-100 rounded-md shadow-lg max-h-60 ring-1 ring-black-100 ring-opacity-5 focus:outline-none sm:text-sm">
-                            {people.map((person, personIdx) => (
-                                <Listbox.Option
-                                    key={personIdx}
-                                    className={({ active }) =>
-                                        `cursor-default select-none relative py-2 pl-10 pr-4 ${active ? 'text-black-100' : 'text-grey-600'
-                                        }`
-                                    }
-                                    value={person}
-                                >
-                                    {({ selected }: any) => (
-                                        <>
-                                            <span
-                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                                    }`}
-                                            >
-                                                {person.name}
-                                            </span>
+  return (
+    <div className="z-40 flex flex-1">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative w-full">
+          <div className="flex flex-1">
+            <Listbox.Button className="flex flex-row w-full items-center py-2  pr-6 font-jost text-left bg-white-100 rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-grey-500 focus-visible:ring-offset-2 focus-visible:border-white-100 text-xs">
+              <span className="pl-2 pt-2 block flex-1  ">
+                <Image
+                  src={selected.imageUrl}
+                  width={20}
+                  height={20}
+                  alt={`flagImage`}
+                ></Image>
+              </span>
+              <div className="block w-full text-center truncate flex-4 pt-1 ">
+                {selected.name}
+              </div>
+              <span className="absolute inset-y-0 top-4 pr-2 pointer-events-none flex-1 right-0">
+                <ChevronDownIcon
+                  className="w-5 h-5 ml-2 -mr-1 text-black-100"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white-100 rounded-md shadow-lg max-h-60 ring-1 ring-black-100 ring-opacity-5 focus:outline-none sm:text-sm">
+              {people.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `cursor-default select-none relative py-2 pl-10 pr-4 ${
+                      active ? "text-black-100" : "text-grey-600"
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected }: any) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
 
-                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
-                                                <Image
-                                                    src={
-                                                        FlagUS
-                                                    }
-                                                    width={20}
-                                                    height={20}
-                                                    alt={`flagImage`}
-                                                ></Image>
-                                            </span>
-
-                                        </>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                        </Listbox.Options>
-                    </Transition>
-                </div>
-            </Listbox>
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
+                        <Image
+                          src={person.imageUrl}
+                          width={20}
+                          height={20}
+                          alt={`flagImage`}
+                        ></Image>
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
-    )
+      </Listbox>
+    </div>
+  );
 }
--- pages/instorefinds/index.tsx
+++ pages/instorefinds/index.tsx
@@ -1,18 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
-import Footer from '../../components/Footer/Footer'
-import Navbar from '../../components/Navbar/Navbar'
-import Calendar from "react-calendar"
-import 'react-calendar/dist/Calendar.css';
-import InstoreCard from '../../components/InstoreFinds/InstoreCard'
-import Dropdown from '../../components/Livedeals/Dropdown'
-import ListBox from '../../components/Livedeals/ListBox'
-import InstoreCardDescription from '../../components/InstoreFinds/InstoreCardDescription'
-import InstoreAddPost from '../../components/InstoreFinds/InstoreAddPost'
-import SearchComponent from '../../components/InstoreFinds/SearchComponent'
+import Image from "next/image";
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import InstoreCard from "../../components/InstoreFinds/InstoreCard";
+import Dropdown from "../../components/Livedeals/Dropdown";
+import ListBox from "../../components/Livedeals/ListBox";
+import InstoreCardDescription from "../../components/InstoreFinds/InstoreCardDescription";
+import InstoreAddPost from "../../components/InstoreFinds/InstoreAddPost";
+import SearchComponent from "../../components/InstoreFinds/SearchComponent";
 
- export default  function InstoreFinds() {
-  const [showdes, setShowdes] = React.useState<boolean>(false)
+export default function InstoreFinds() {
+  const [showdes, setShowdes] = React.useState<boolean>(false);
   const [post, setPost] = React.useState(false);
   const [value, onChange] = React.useState(new Date());
   return (
@@ -21,77 +21,118 @@ import SearchComponent from '../../components/InstoreFinds/SearchComponent'
         <Navbar />
       </div>
       <div className="relative m-2 space-y-2 xl:hidden">
-        {
-          post ? <InstoreAddPost setPost={setPost} />
-            : showdes ? <InstoreCardDescription setShowdes={setShowdes} /> : <><div className="flex justify-between mx-4 mt-4 flex-row mb-4">
+        {post ? (
+          <InstoreAddPost setPost={setPost} />
+        ) : showdes ? (
+          <InstoreCardDescription setShowdes={setShowdes} />
+        ) : (
+          <>
+            <div className="flex justify-between mx-4 mt-4 flex-row mb-4">
               <div className="flex-col mb-4">
-                <div className="font-jost text-lg text-black-100">Instore Finds</div>
-                <div className="font-jost text-sm text-grey-200">Feb 16, 2022</div>
+                <div className="font-jost text-lg text-black-100">
+                  Instore Finds
+                </div>
+                <div className="font-jost text-sm text-grey-200">
+                  Feb 16, 2022
+                </div>
               </div>
               <div>
-                <button onClick={() => setPost(true)} className="bg-blue-300 mt-1 w-24 h-8  flex flex-row space-x-2 justify-center  font-jost text-sm text-white-100 rounded-md">
-                  <div className=''><Image
-                    src={require("../../assets/instorefinds/add.svg")}
-                    width={18}
-                  ></Image></div>
+                <button
+                  onClick={() => setPost(true)}
+                  className="bg-blue-300 mt-1 w-24 h-8  flex flex-row space-x-2 justify-center  font-jost text-sm text-white-100 rounded-md"
+                >
+                  <div className="">
+                    <Image
+                      src={require("../../assets/instorefinds/add.svg")}
+                      width={18}
+                      alt="alt"
+                    ></Image>
+                  </div>
                   <div className="pt-1">Post</div>
                 </button>
               </div>
             </div>
-              <div className="xl:hidden flex flex-1 justify-between space-x-4 mb-6 z-40">
-                <div className="flex flex-1"><Dropdown /></div>
-                <div className="flex flex-1"><ListBox /></div>
+            <div className="xl:hidden flex flex-1 justify-between space-x-4 mb-6 z-40">
+              <div className="flex flex-1">
+                <Dropdown />
               </div>
-              <InstoreCard isdes={false} setShowdes={setShowdes} />
-              <InstoreCard isdes={true} setShowdes={setShowdes} />
-              <InstoreCard isdes={false} setShowdes={setShowdes} />
-              <InstoreCard isdes={true} setShowdes={setShowdes} /></>
-        }
-
+              <div className="flex flex-1">
+                <ListBox />
+              </div>
+            </div>
+            <InstoreCard isdes={false} setShowdes={setShowdes} />
+            <InstoreCard isdes={true} setShowdes={setShowdes} />
+            <InstoreCard isdes={false} setShowdes={setShowdes} />
+            <InstoreCard isdes={true} setShowdes={setShowdes} />
+          </>
+        )}
       </div>
       <div className="relative hidden xl:flex flex-1 flex-row m-2 mb-10">
-        <div className='w-1/3 h-full flex flex-col  from-zinc-300 justify-center'>
-        <div className="flex flex-1 mx-4 mt-10 w-80" >
-      <Calendar onChange={onChange} next2Label={null} prev2Label={null} className={'bg-white-100 rounded-xl'} value={value} />
-    </div>
-    <div className="flex flex-1 mx-4 mt-10"><SearchComponent/></div>
-        <div className="flex flex-1  mx-4 mt-10 w-80"><Dropdown /></div>
+        <div className="w-1/3 h-full flex flex-col  from-zinc-300 justify-center">
+          <div className="flex flex-1 mx-4 mt-10 w-80">
+            <Calendar
+              onChange={onChange}
+              next2Label={null}
+              prev2Label={null}
+              className={"bg-white-100 rounded-xl"}
+              value={value}
+            />
+          </div>
+          <div className="flex flex-1 mx-4 mt-10">
+            <SearchComponent />
+          </div>
+          <div className="flex flex-1  mx-4 mt-10 w-80">
+            <Dropdown />
+          </div>
         </div>
-        <div className='w-2/3 h-full flex flex-col mx-24  justify-center pt-4'>
-        <div className="flex  flex-1 items-center justify-between flex-row space-x-4">
-              <div className="flex-col flex flex-3">
-                <div className="font-jost text-8xl text-black-100">Instore Finds</div>
-                <div className="font-jost text-xl text-grey-200">Feb 16, 2022</div>
+        <div className="w-2/3 h-full flex flex-col mx-24  justify-center pt-4">
+          <div className="flex  flex-1 items-center justify-between flex-row space-x-4">
+            <div className="flex-col flex flex-3">
+              <div className="font-jost text-8xl text-black-100">
+                Instore Finds
               </div>
-              <div className="flex flex-2 items-center"><ListBox /></div>
-              <div className="flex flex-1 justify-end items-start">
-                <button onClick={() => {}} className="bg-blue-300 mt-1 w-24 h-8  flex flex-row space-x-2 justify-center  font-jost text-sm text-white-100 rounded-md">
-                  <div className=''><Image
+              <div className="font-jost text-xl text-grey-200">
+                Feb 16, 2022
+              </div>
+            </div>
+            <div className="flex flex-2 items-center">
+              <ListBox />
+            </div>
+            <div className="flex flex-1 justify-end items-start">
+              <button
+                onClick={() => {}}
+                className="bg-blue-300 mt-1 w-24 h-8  flex flex-row space-x-2 justify-center  font-jost text-sm text-white-100 rounded-md"
+              >
+                <div className="">
+                  <Image
                     src={require("../../assets/instorefinds/add.svg")}
                     width={18}
-                  ></Image></div>
-                  <div className="pt-1">Post</div>
-                </button>
-              </div>
-         </div>
+                    alt="alt"
+                  ></Image>
+                </div>
+                <div className="pt-1">Post</div>
+              </button>
+            </div>
+          </div>
 
-         <div className="flex flex-3 flex-col mt-8 space-y-8">
-          <InstoreCard isdes={false} setShowdes={setShowdes} />
-         <InstoreCard isdes={true} setShowdes={setShowdes}/>
-         <InstoreCard isdes={false} setShowdes={setShowdes} />
-         <InstoreCard isdes={true} setShowdes={setShowdes}/>
-         </div>
-         </div>
-         
-        <div className='w-1/3 h-full '>
-        <InstoreCardDescription setShowdes={setShowdes} />
+          <div className="flex flex-3 flex-col mt-8 space-y-8">
+            <InstoreCard isdes={false} setShowdes={setShowdes} />
+            <InstoreCard isdes={true} setShowdes={setShowdes} />
+            <InstoreCard isdes={false} setShowdes={setShowdes} />
+            <InstoreCard isdes={true} setShowdes={setShowdes} />
+          </div>
+        </div>
+
+        <div className="w-1/3 h-full ">
+          <InstoreCardDescription setShowdes={setShowdes} />
         </div>
       </div>
-      <div className="b
-            sis-1/4">
+      <div
+        className="b
+            sis-1/4"
+      >
         <Footer />
       </div>
     </div>
-  )
+  );
 }
-
